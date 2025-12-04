b456ecefcae100551e95224a12867dc87f3e1b22
Villacus <oiervillanueva2@gmail.com>
2025-12-04 16:43:16 +0100

    Añadiendo echos al compilador

--- compExek.sh
+++ compExek.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Compilatu"
 gcc -O2 -o multzohitz_s multzohitz_s.c funth_s.c -lm #Compilatu
 
+echo "Exekutatu"
 ./multzohitz_s ../ARK/3-proiektua/hitzbektoreak.dat ../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
 
+echo "Konparatu"
 diff emaitzak.out emaitzak1000.out
