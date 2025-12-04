07c09783e2a8378b03c63667634b7bafdfa041e3
Villacus <oiervillanueva2@gmail.com>
2025-12-04 14:45:49 +0100

    Compilador con el diff

--- compExek.sh
+++ compExek.sh
@@ -3,3 +3,5 @@
 gcc -O2 -o multzohitz_s multzohitz_s.c funth_s.c -lm #Compilatu
 
 ./multzohitz_s ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
+
+diff emaitzak.out emaitzak1000.out
