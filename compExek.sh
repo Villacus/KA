9d520e7ca2c5db9b185cc9f0738023085b3bb384
Villacus <oiervillanueva2@gmail.com>
2025-12-04 10:35:28 +0100

    Corregido el compilador

--- compExek.sh
+++ compExek.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -2 -o multzohitz_s multzohitz_s.c funth_s.c -lm #Compilatu
+gcc -O2 -o multzohitz_s multzohitz_s.c funth_s.c -lm #Compilatu
 
 ./multzohitz_s ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
