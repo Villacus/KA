68f16459a715b9026f796f68ba0b684c25832fe8
Villacus <oiervillanueva2@gmail.com>
2025-12-04 15:21:06 +0100

    Corrección directorio compilador

--- compExek.sh
+++ compExek.sh
@@ -2,4 +2,4 @@
 
 gcc -O2 -o multzohitz_s multzohitz_s.c funth_s.c -lm #Compilatu
 
-./multzohitz_s ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
+./multzohitz_s ../ARK/3-proiektua/hitzbektoreak.dat ../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
