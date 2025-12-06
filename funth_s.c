73d63fd9cc41638625753871ae81dc508accfce7
Villacus <oiervillanueva2@gmail.com>
2025-12-06 10:00:54 +0100

    Añadido print en max

--- funth_s.c
+++ funth_s.c
@@ -176,6 +176,7 @@ void ztalorren_analisia (struct multzoinfo *kideak, float alor[][ALORRA], struct
         }
         if (mediana>alordist[a].mmax) {
           alordist[a].mmax = mediana;
+          printf("%f\n", alordist[a].mmax);
           alordist[a].multzomax = k;
         }
       }
