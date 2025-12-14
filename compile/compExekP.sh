101ab831d8edad30feb731a2598145d319d3bebd
Villacus <oiervillanueva2@gmail.com>
2025-12-14 12:31:45 +0100

    Cambio compilador

--- compile/compExekP.sh
+++ compile/compExekP.sh
@@ -4,7 +4,7 @@ echo "Compilatu"
 gcc -O2 -fopenmp -o ../multzohitz_p ../src/paralelo/multzohitz_p.c ../src/paralelo/funth_p.c -lm #Compilatu
 
 echo "Exekutatu"
-../multzohitz_p ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
+../multzohitz_p ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat
 
 echo "Konparatu"
 diff ../emaitzak/emaitzak_p.out ../../ARK/3-proiektua/emaitzak.out
--- compile/compExekS.sh
+++ compile/compExekS.sh
@@ -4,7 +4,7 @@ echo "Compilatu"
 gcc -O2 -o ../multzohitz_s ../src/serie/multzohitz_s.c ../src/serie/funth_s.c -lm #Compilatu
 
 echo "Exekutatu"
-../multzohitz_s ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat 1000 #soilik 1000 hitzekin
+../multzohitz_s ../../ARK/3-proiektua/hitzbektoreak.dat ../../ARK/3-proiektua/ztalor.dat
 
 echo "Konparatu"
 diff ../emaitzak/emaitzak_s.out ../../ARK/3-proiektua/emaitzak.out
