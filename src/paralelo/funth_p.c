4837b62170b7344e8bad260ae1ba69d8beace2ce
Villacus <oiervillanueva2@gmail.com>
2025-12-15 20:18:19 +0100

    Revert "mejor?"
    This reverts commit 88dfb842a32a8c2db8100257018d02ff78df029c.

--- src/paralelo/funth_p.c
+++ src/paralelo/funth_p.c
@@ -91,22 +91,20 @@ double balidazioa (float hitz[][ALDAKOP], struct multzoinfo *kideak, float zent[
   double baturas = 0.0;
 
 
-  #pragma omp parallel private (k, i, j, ind_x)
-  {
-    for (k=0;k<multzokop;k++) {
-      double batura = 0.0;
-      if (kideak[k].kop>1) {
-        for (i=0;i<kideak[k].kop;i++) {
-          ind_x = kideak[k].osagaiak[i];
-          #pragma omp parallel for reduction(+:batura)
-          for (j=i+1;j<kideak[k].kop;j++) {
-            batura += 2*hitzen_distantzia(hitz[ind_x],hitz[kideak[k].osagaiak[j]]);
-          }
+
+  for (k=0;k<multzokop;k++) {
+    double batura = 0.0;
+    if (kideak[k].kop>1) {
+      for (i=0;i<kideak[k].kop;i++) {
+        ind_x = kideak[k].osagaiak[i];
+        #pragma omp parallel for schedule(dynamic, 1) private(k, i, j, ind_x) reduction(+:batura)
+        for (j=i+1;j<kideak[k].kop;j++) {
+          batura += 2*hitzen_distantzia(hitz[ind_x],hitz[kideak[k].osagaiak[j]]);
         }
-        multzo_trinko[k] = batura/(kideak[k].kop*(kideak[k].kop-1));
-      } else {
-        multzo_trinko[k] = 0.0;
       }
+      multzo_trinko[k] = batura/(kideak[k].kop*(kideak[k].kop-1));
+    } else {
+      multzo_trinko[k] = 0.0;
     }
   }
 
